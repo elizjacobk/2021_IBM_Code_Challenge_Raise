398f623ca2291a653092dbb5e1ea895216a8a94e
AneezMuhammed <aneezmuhammed01@gmail.com>
2021-11-24 14:31:01 +0530

    added signup.php

---
+++ signup.php
@@ -0,0 +1,49 @@
+<?php
+      session_start();
+
+      $servername = "localhost";
+$database = "raise";
+$username = "root";
+$password = "";
+    //   $db = mysqli_connect('localhost', 'designproject', '', 'Rafiki');
+    // $db = mysqli_connect('localhost', 'root', '', '','');
+      $db = mysqli_connect($serv, $username, $password, $database);
+
+      //variables
+      $username;
+      $password;
+      $email;
+      $fullname;
+      $errors = array(); 
+
+      if (isset($_POST['Signup'])){
+            //accepting
+              $username = mysqli_real_escape_string($db, $_POST['Username']);
+              $password = mysqli_real_escape_string($db, $_POST['Password']);
+              $fullname=  mysqli_real_escape_string($db, $_POST['Fullname']);
+              $email=   mysqli_real_escape_string($db, $_POST['Email']);  
+
+            //error checking
+            if(empty($username)){array_push($errors, "User name is required");}
+            if (empty($password)) { array_push($errors, "Password is required"); }
+            if (empty($email)) { array_push($errors, "Email is required"); }
+            if (empty($fullname)) { array_push($errors, "Full name required"); }
+
+            //connection
+            if (count($errors) == 0) {
+                    $sql="INSERT INTO user(fullname,email,username,password) VALUES($fullname,$email,$username,$password)";
+                //   $query="SELECT * FROM customer where email='$loguser' AND Password='$logpass';";
+                //   $results = mysqli_query($db, $sql);
+                  if(mysqli_query($db,$sql)){
+                      echo "New record created "
+                      $_SESSION['username'] = $username;
+                      $_SESSION['success'] = "You are now logged in";
+                      header('location: index.html');
+                  }
+                  else{
+                      mysqli_error($conn);
+                  }
+                // 
+                }
+      }
+      ?>
