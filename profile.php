f5788abdf44e0f0e36d62cf983f5436358d05f8c
Salvin Antony Varghese <70530234+salvinanto7@users.noreply.github.com>
2021-11-25 15:38:27 +0530

    added profile.php

---
+++ profile.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,600;1,100;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/navlogo.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Raise.</title>
+  </head>
+  <body>
+    <section class="cart-nav">
+    
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="nav-img" href="#navbar"></a>
+        <a class="navbar-brand" href="#"><img class="nav-logo" src="images/navlogo.png" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-el nav-link" href="blog.php">Blog<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-el nav-link" href="login.php">Account</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-el nav-link" href="#">Profile</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</section>
+      <br><br><br>
+      <a class= "try" href="add.php">Add post</a>
+    <br><br><br>
+    <h3 class="h3 beep">&emsp;&emsp;&emsp;Your posts</h3>
+        <hr>
+    <section class="container ">
+      <div class="prod-cont row">
+        <div class="row">
+          <div class="col span-1-of-2">
+              <div class="product-grid2">
+                  <div class="product-image2">
+                      <a href="#">
+                          <img class="pic-1" src="images/6.jpeg">
+                          <img class="pic-2" src="images/6.jpeg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                            </svg>
+                          </a></li>
+                      </ul>
+                      <a class="add-to-cart" href="">Open Blog</a>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">The fab femme</a></h3>
+                      <span class="price">Coming out as non-binary was spiritual for me. I meditate everyday, and one day during a meditation, I had this image of myself walking into a family event and feeling my family perceive me as non-binary - as not being adhered to any gender.  I started crying because I felt so freed <u>Cont..</u></span>
+                  </div>
+              </div>
+          </div>
+          <div class="col span-1-of-2">
+            <div class="product-grid2">
+                <div class="product-image2">
+                    <a href="#">
+                        <img class="pic-1" src="images/6.jpeg">
+                        <img class="pic-2" src="images/6.jpeg">
+                    </a>
+                    <ul class="social">
+                        <li><a href="#">
+                          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                          </svg>
+                        </a></li>
+                    </ul>
+                    <a class="add-to-cart" href="">Open Blog</a>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">The fab femme</a></h3>
+                    <span class="price">Coming out as non-binary was spiritual for me. I meditate everyday, and one day during a meditation, I had this image of myself walking into a family event and feeling my family perceive me as non-binary - as not being adhered to any gender.  I started crying because I felt so freed <u>Cont..</u></span>
+                </div>
+            </div>
+        </div><div class="prod-cont row">
+          <div class="row">
+            <div class="col span-1-of-2">
+                <div class="product-grid2">
+                    <div class="product-image2">
+                        <a href="#">
+                            <img class="pic-1" src="images/6.jpeg">
+                            <img class="pic-2" src="images/6.jpeg">
+                        </a>
+                        <ul class="social">
+                            <li><a href="#">
+                              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                              </svg>
+                            </a></li>
+                        </ul>
+                        <a class="add-to-cart" href="">Open Blog</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="#">The fab femme</a></h3>
+                        <span class="price">Coming out as non-binary was spiritual for me. I meditate everyday, and one day during a meditation, I had this image of myself walking into a family event and feeling my family perceive me as non-binary - as not being adhered to any gender.  I started crying because I felt so freed <u>Cont..</u></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col span-1-of-2">
+              <div class="product-grid2">
+                  <div class="product-image2">
+                      <a href="#">
+                          <img class="pic-1" src="images/6.jpeg">
+                          <img class="pic-2" src="images/6.jpeg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                            </svg>
+                          </a></li>
+                      </ul>
+                      <a class="add-to-cart" href="">Open Blog</a>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">The fab femme</a></h3>
+                      <span class="price">Coming out as non-binary was spiritual for me. I meditate everyday, and one day during a meditation, I had this image of myself walking into a family event and feeling my family perceive me as non-binary - as not being adhered to any gender.  I started crying because I felt so freed <u>Cont..</u></span>
+                  </div>
+              </div>
+          </div>
+        </section>
+    <footer>
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12 segment-one">
+              <img class="footer-logo" src="images/logo1.png" alt="sloth! psht">
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 segment-two">
+              <h3>Raise.</h3>
+              <p>Rainbows Reign.</p>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 segment-three">
+              <h2>Features</h2>
+              <ul>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Profile</a></li>
+              </ul>
+              </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <script>
+      $(window).scroll(function(){
+      $('nav').toggleClass('scrolled', $(this).scrollTop() > 25);
+      });
+     </script>
+    </body>
+  </html>
