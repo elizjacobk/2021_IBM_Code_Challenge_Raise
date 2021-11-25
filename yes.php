d4e43c0871a8983f317d5b6e2a519f063f245ace
Salvin Antony Varghese <70530234+salvinanto7@users.noreply.github.com>
2021-11-25 15:37:32 +0530

    added yes.php

---
+++ yes.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <title>Add blog</title>
+    <link rel="stylesheet" href="css/prof.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,600;1,100;1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <a href="index.php">Done, redirect</a>
+</body>
+</html>
